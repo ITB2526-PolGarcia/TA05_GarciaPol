a8350eb3e6df140462e324e137e867a89031bb52
pol.garcia.7e7 <pol.garcia.7e7@itb.cat>
2026-01-12 11:47:09 +0100

    12/01/26 - Prueba de disp moviles

---
+++ js/languages.js
@@ -0,0 +1,32 @@
+{
+  "ca": {
+    "nav_home": "Inici",
+    "nav_projects": "Projectes",
+    "nav_contact": "Contacte",
+    "intro_name": "Pol García",
+    "intro_role": "Cyber Admin",
+    "intro_btn": "Contacta'm",
+    "featured_title": "Projectes Destacats",
+    "learn_more": "Saber-ne més"
+  },
+  "es": {
+    "nav_home": "Inicio",
+    "nav_projects": "Proyectos",
+    "nav_contact": "Contacto",
+    "intro_name": "Pol García",
+    "intro_role": "Cyber Admin",
+    "intro_btn": "Contactar",
+    "featured_title": "Proyectos Destacados",
+    "learn_more": "Saber más"
+  },
+  "en": {
+    "nav_home": "Home",
+    "nav_projects": "Projects",
+    "nav_contact": "Contact",
+    "intro_name": "Pol García",
+    "intro_role": "Cyber Admin",
+    "intro_btn": "Get in touch",
+    "featured_title": "Featured Projects",
+    "learn_more": "Learn More"
+  }
+}
